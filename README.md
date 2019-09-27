47f695c069c3791093de88886859d5c08ca05717
SethuRam82 <sethu.lingam@gmail.com>
2019-09-27 15:06:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-uuqevn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-uuqevn)
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+
+@Component(
+    {
+        selector: 'pm-products',
+        templateUrl: './product-list.component.html',
+        styleUrls: ['./product-list.component.css']
+    }
+)
+export class ProductListComponent implements OnInit {
+    pageTitle: String = "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    _listFilter: string;
+
+    get listFilter(): string {
+      return this._listFilter;
+    }
+
+    set listFilter(value: string){
+      this._listFilter=value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+      }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
+        {
+            "productID": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 23.99,
+            "starRating": 4.3,
+            "imageUrl": "https://openclipart.org/image/300px/garden_cart.png"
+        },
+        {
+            "productID": 5,
+            "productName": "Hammer",
+            "productCode": "GDN-0012",
+            "releaseDate": "Apri 18, 2017",
+            "description": "curved claw steel hammer",
+            "price": 3.99,
+            "starRating": 3.2,
+            "imageUrl": "https://openclipart.org/image/300px/hammer.png"
+        }
+    ];
+    
+    constructor()
+    {
+      this.filteredProducts = this.products;
+      this.listFilter = 'cart';
+    }
+    performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+      ngOnInit(): void {
+        console.log("On Init");
+      }
+     
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    onRatingClicked(message: string): void {
+      this.pageTitle='Produc List' + message;
+    }
+}
