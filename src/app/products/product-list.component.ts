d32a43c8dc8cf4e6f345ad9b0ac53195bbad60cc
SethuRam82 <sethu.lingam@gmail.com>
2019-09-27 22:19:09 +0530

    3 files modified

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
     _listFilter: string;
 
     get listFilter(): string {
@@ -61,10 +62,18 @@ export class ProductListComponent implements OnInit {
 
       ngOnInit(): void 
       {
-        this.products = this.productService.getProducts();
-          this.filteredProducts = this.products;
+        // this.products = this.productService.getProducts();
+        this.productService.getProducts().subscribe(
+          products => 
+          {
+            this.products = products;
+            this.filteredProducts = this.products;
+          },
+            error => this.errorMessage =<any>error
+        );
+          
       // this.listFilter = 'cart';
-        console.log("On Init");
+        // console.log("On Init");
       }
      
     toggleImage(): void {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
 
 
 @Injectable( {
@@ -7,31 +10,56 @@ import { IProduct } from './product';
 }
 )
 export class ProductService {
+  private productUrl = 'D:/products.json';
+  // private productUrl =  'https://27th-sep.stackblitz.io/api/products/products.json';
+  // private productUrl = 'api/products/products.json'; 
 
-getProducts(): IProduct[]
+constructor(private http: HttpClient)
 {
-  return [
-    {
-            "productID": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 23.99,
-            "starRating": 4.3,
-            "imageUrl": "https://openclipart.org/image/300px/garden_cart.png"
-    },
-    {
-            "productID": 5,
-            "productName": "Hammer",
-            "productCode": "GDN-0012",
-            "releaseDate": "Apri 18, 2017",
-            "description": "curved claw steel hammer",
-            "price": 3.99,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/hammer.png"
-    }
+
+}
+
+getProducts(): Observable<IProduct[]>
+{
+  return this.http.get<IProduct[]>
+  (this.productUrl).pipe(tap (data => console.log("All:" + JSON.stringify(data))),
+    catchError(this.handleError));
+
+}
+  // return [
+    // {
+    //         "productID": 2,
+    //         "productName": "Garden Cart",
+    //         "productCode": "GDN-0023",
+    //         "releaseDate": "March 18, 2016",
+    //         "description": "15 gallon capacity rolling garden cart",
+    //         "price": 23.99,
+    //         "starRating": 4.3,
+    //         "imageUrl": "https://openclipart.org/image/300px/garden_cart.png"
+    // },
+    // {
+    //         "productID": 5,
+    //         "productName": "Hammer",
+    //         "productCode": "GDN-0012",
+    //         "releaseDate": "Apri 18, 2017",
+    //         "description": "curved claw steel hammer",
+    //         "price": 3.99,
+    //         "starRating": 3.2,
+    //         "imageUrl": "https://openclipart.org/image/300px/hammer.png"
+    // }
     
-  ]
+  // ]
+private handleError (err: HttpErrorResponse)
+{
+  let errorMessage ='';
+  if(err.error instanceof ErrorEvent)
+  {errorMessage = `An error occured :${err.error.message}`;
+  }
+  else 
+  {
+    errorMessage = `Server returned code: ${err.status}, error message is:${err.message}`;
+  }
+  console.error(errorMessage);
+  return throwError(errorMessage);
 }
 }
